58e823c900a9923941c1b5f5abf6de63c062ce51
Chad Lynch <chyad@hotmail.com>
2023-02-10 01:48:22 -0500

    chore: Change typechain import

--- scripts/cycle.ts
+++ scripts/cycle.ts
@@ -1,6 +1,6 @@
 import { ethers } from "hardhat";
 import { localhost } from "./networks";
-import { LoreumNFT, LoreumNFT__factory } from "../typechain-types";
+import { LoreumNFT, LoreumNFT__factory } from "../typechain";
 
 async function main() {
   /*
